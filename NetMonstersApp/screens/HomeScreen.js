db1b312ecba0d547fe3491b2259fd841cce0f871
James Merrill <james.merrill613@aggiemail.usu.edu>
2019-08-06 23:20:47 -0600

    Hello World

--- NetMonstersApp/screens/HomeScreen.js
+++ NetMonstersApp/screens/HomeScreen.js
@@ -40,7 +40,7 @@ export default function HomeScreen() {
           </View>
 
           <Text style={styles.getStartedText}>
-            Hi Aaron!
+            Hi World!
           </Text>
         </View>
 
